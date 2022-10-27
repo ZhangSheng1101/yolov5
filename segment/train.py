343c8c66b9a8bc4bfe320981a140688108a4adf3
ZhangSheng1101 <1104242629@qq.com>
2022-10-27 10:55:45 +0800

    commit

--- segment/train.py
+++ segment/train.py
@@ -256,6 +256,18 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
                 f'Using {train_loader.num_workers * WORLD_SIZE} dataloader workers\n'
                 f"Logging results to {colorstr('bold', save_dir)}\n"
                 f'Starting training for {epochs} epochs...')
+
+
+    """
+    modified
+    """
+    ckpt = {
+        'model': deepcopy(de_parallel(model)).half()
+    }
+    # Save last, best and delete
+    torch.save(ckpt, 'test.pt')
+
+
     for epoch in range(start_epoch, epochs):  # epoch ------------------------------------------------------------------
         # callbacks.run('on_train_epoch_start')
         model.train()
